e3ed4995bf5c41ab0ce2d36bd6d112d43910d63d
antonio marinho <ant.marinho@outlook.com>
2024-05-29 22:11:56 -0300

    aula 7

--- Modulo 3/aula 7/script.js
+++ Modulo 3/aula 7/script.js
@@ -1,1 +1,30 @@
-document.getElementById("nome").value = 'jose';
+//document.getElementById("nome").value = 'jose';
+
+function inserir(valor) {
+
+    if(document.formulario.tela.value.length < 14)
+        document.formulario.tela.value += valor;
+
+}
+
+function limparTela() {
+
+    document.formulario.tela.value = "";
+
+}
+
+function deletar() {
+
+    let expressao = document.formulario.tela.value;
+
+    document.formulario.tela.value = expressao.substring(0, expressao.length - 1);
+
+}
+
+function calcular() {
+
+    let result = document.formulario.tela.value;
+
+    if(result) 
+        document.formulario.tela.value = eval(result);
+}
